67092a3a55124d84fdc9d3628ad4fb4e719ed477
Andres Quast <quast@hbz-nrw.de>
2019-10-31 10:38:35 +0100

    first content commit

---
+++ Json2Java/src/main/java/de/nrw/hbz/json/helper/json2java/MappingEvaluator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.nrw.hbz.json.helper.json2java;
+
+/**
+ * @author aquast
+ *
+ */
+public class MappingEvaluator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
