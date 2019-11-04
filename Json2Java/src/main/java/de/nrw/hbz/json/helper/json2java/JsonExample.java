fab3f84797f1176b6983099b4bb2775f02523f13
Andres Quast <quast@hbz-nrw.de>
2019-11-04 23:01:17 +0100

    work on json structures

--- Json2Java/src/main/java/de/nrw/hbz/json/helper/json2java/JsonExample.java
+++ Json2Java/src/main/java/de/nrw/hbz/json/helper/json2java/JsonExample.java
@@ -20,7 +20,12 @@ public class JsonExample {
 	public static void main(String[] args) {
 		JsonNode node = null; 
 		String jsonExample1 = new String();
-		jsonExample1 = "{\"vorname\" : \"Andres\", \"nachname\" : \"Quast\" , \"alter\" : 53, \"wohnorte\" : [\"Köln\", \"Bochum\", \"Göttingen\" ] }";
+		jsonExample1 = "{\"vorname\" : \"Andres\", \"nachname\" : \"Quast\" , \"alter\" : 53, \"wohnorte\" : [\"Bochum\", \"Göttingen\" ], "
+				+ "\"anstellungen\" : [{ \"anstellung\" : {\"ort\" : \"Köln\", \"arbeitgeber\" : \"hbz\", \"von\" : 2008 , \"bis\" : \"laufend\" }"
+				+ ", \"anstellung\" : {\"ort\" : \"Göttingen\", \"arbeitgeber\" : \"SUB\", \"von\" : 2005 , \"bis\" : 2008 }"
+				+ ", \"anstellung\" : {\"ort\" : \"Göttingen\", \"arbeitgeber\" : \"MiniCar\", \"von\" : 2003 , \"bis\" : 2005 } "
+				+ "}]"
+				+ ", \"akademischeTitel\" : [ \"Dipl.-Geol.\", \"Dr.\"]}";
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			node = mapper.readTree(jsonExample1);
--- Json2Java/src/main/java/de/nrw/hbz/json/helper/json2java/StructureFinder.java
+++ Json2Java/src/main/java/de/nrw/hbz/json/helper/json2java/StructureFinder.java
@@ -3,8 +3,11 @@
  */
 package de.nrw.hbz.json.helper.json2java;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Map.Entry;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -15,22 +18,61 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class StructureFinder {
 
 	Hashtable<String,String> simpleElements = new Hashtable<>();
+	// Hashtable<String, Hashtable<String,String>> complexElement = new Hashtable<>(); 
+	ArrayList<Hashtable<String,String>> complexElement = new ArrayList<>();
+	StringBuffer pathBuffer = new StringBuffer("");
 
 	public void mapStructure(JsonNode node) {
-		StringBuffer sb = new StringBuffer("");
+		mapStructure(node, new StringBuffer("."));
+		printElements();
+	}
+	
+	public void mapStructure(JsonNode node, StringBuffer pBuffer) {
+
 		Iterator<String> it = node.fieldNames();
 		while(it.hasNext()) {
+			
 			String key = it.next();
 			if (node.get(key).isValueNode()){
-				System.out.println(key + " : " + node.get(key).asText() );
-				simpleElements.put(key, node.get(key).asText());
-			}else if(node.get(key).isArray()){
-				System.out.println(key + " : " + node.get(key).asText() );
-				//node.get(key).isArray();
-				sb.append(key + ".");
-				JsonNode complexNode = node.findPath(key);
-				mapStructure(complexNode);
+				Hashtable<String, String> iE = new Hashtable<>();
+				iE.put(pBuffer + key, node.get(key).asText());
+				complexElement.add(iE);
+			}
+			
+			if(node.get(key).isObject()){
+				System.out.println(key + " : " + node.get(key).size() );
+				Iterator<Entry<String, JsonNode>> nIt = node.get(key).fields();
+				//while(nIt.hasNext()) {
+				//	System.out.println("key : " + nIt.next().getKey());
+				//}
+				//if (subNode.size() > 1) {
+					//for (int i=0; i < subNode.size(); i++) {
+						//pBuffer.append(key + ".");
+					//	System.out.println(subNode.toString());
+						// JsonNode complexNode = subNode.get(i);
+						//mapStructure(subNode.get(i), pBuffer);
+					//}
+				//}
+				pBuffer.append(key + ".");
+				JsonNode complexNode = node.get(key);
+				mapStructure(complexNode, pBuffer);
+			}
+			if(node.get(key).isArray()){
+				System.out.println(key + " : " + node.get(key).size() );
 			}
 		}
-	};
+	}
+	
+	public void printElements() {
+		Iterator<Hashtable<String,String>> it = complexElement.iterator();
+		while (it.hasNext()) {
+			Hashtable<String,String> simpleLiterals = it.next();
+			Enumeration<String> sEnum = simpleLiterals.keys();
+			while (sEnum.hasMoreElements()){
+				String key = sEnum.nextElement();
+				System.out.println(key + " : " + simpleLiterals.get(key));
+			}
+		}
+	}
+
 }
